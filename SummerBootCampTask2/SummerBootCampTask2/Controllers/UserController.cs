66b429adbe178452ad3d0b97d6d8beb27a90d727
kdobrovolsky <87859939+kdobrovolsky@users.noreply.github.com>
2021-07-23 14:51:12 +0300

    YEYEY
    OKSY

---
+++ SummerBootCampTask2/SummerBootCampTask2/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SummerBootCampTask2.Contexts;
+using System.Linq;
+
+namespace SummerBootCampTask2.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly BootCampDbContext dbContext;
+
+        public UserController(BootCampDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var users = dbContext.Users.ToList();
+            if (User.Identity.IsAuthenticated)
+            {
+                users = users.Where(x => x.Email != User.Identity.Name).ToList();
+            }
+            return View(users);
+        }
+    }
+}
